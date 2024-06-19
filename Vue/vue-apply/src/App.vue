8f7f403737675dc8f9f7348dd342ad57f2e1f5cb
라 희 <105037764+r6hex-1g@users.noreply.github.com>
2024-06-19 12:44:24 +0900

    [ADD] : 📗 Vue part 3-1 add

--- Vue/vue-apply/src/App.vue
+++ Vue/vue-apply/src/App.vue
@@ -6,7 +6,13 @@ export default {
       check: [],
       agree: "no",
       name2: "",
-      tier: 0
+      tier: 0,
+      contacts: [
+        { no: 1011, name: "RM", tel: "010-3456-8299" },
+        { no: 1012, name: "정국", tel: "010-3456-8298" },
+        { no: 1013, name: "제이홉", tel: "010-3456-8297" },
+        { no: 1014, name: "슈가", tel: "010-3456-8296" },
+      ],
     };
   },
 
@@ -66,10 +72,50 @@ export default {
       <span v-else-if="tier >= 10">Bronze</span>
       <span v-else>iron</span> 
     </div>
+    <br>
 
+    <div>
+      <table id="list">
+      <thead>
+        <tr>
+          <th>번호</th>
+          <th>이름</th>
+          <th>전화번호</th>
+        </tr>
+      </thead>
+      <tbody id="contacts">
+        <tr v-for="contact in contacts" :key="contact.no">
+          <!-- :key="contact.no는 v-bind:key의 생략형태이며,
+             key속성값은 고유값으로 작성되며 외부에서는 보이지 않는다. -->
+          <td>{{contact.no}}</td>
+          <td>{{contact.name}}</td>
+          <td>{{contact.tel}}</td>
+        </tr>
+        <!-- 반복이 되려는 템플릿 1셋트를 적어놓고, for...in를 이용하여
+           데이터의 반복을 구현한다. 각 개별데이터를 입력해야하는 구간에는
+           객체의 점표기법, 대괄호표기법을 활용하여 머스태시로 입력해두면
+           초기렌더링때 구현이 된다. -->
+      </tbody>
+    </table>
+    </div>
   </div>
 </template>
 
 <style scoped>
+#list {
+      width: 600px;
+      border: 1px solid black;
+      border-collapse: collapse;
+    }
 
+    #list td,
+    #list th {
+      border: 1px solid black;
+      text-align: center;
+    }
+
+    #list>thead>tr {
+      color: yellow;
+      background-color: purple;
+    }
 </style>
